491dfd3066b9f019b4dd71d399774ee6011ac317
Lucas Melo <145815917+LuskGPM@users.noreply.github.com>
2026-04-15 00:12:54 -0300

    para aula

---
+++ TkinterExemplos/estrutura_absoluta.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+tk.Button(root, text="Clique").place(x=50, y=100)
+
+root.mainloop()
--- TkinterExemplos/estrutura_simples.py
+++ TkinterExemplos/estrutura_simples.py
@@ -5,7 +5,7 @@ janela.title("Estrutura Simples")
 janela.geometry("300x400")
 
 tk.Label(janela, text="Olá, Mundo!").pack(fill="x")
-tk.Button(janela, text="Clique Aqui", command=lambda: print("Botão clicado!")).pack(side="left")
+tk.Button(janela, text="Clique Aqui").pack(side="left")
 tk.Button(janela, text="Sair", command=janela.quit).pack(side="right")
 tk.Entry(janela).pack(pady=10)
 tk.Label(janela, text="Tkinter é fácil de usar!").pack(side="bottom")
