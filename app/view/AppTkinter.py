8040c80a54b1c80c2fd01c5c6b497eaf91bfc9dc
Lucas Melo <lucasgpm00@gmail.com>
2026-04-19 22:41:37 -0300

    corrijindo leitura

--- app/view/AppTkinter.py
+++ app/view/AppTkinter.py
@@ -4,14 +4,15 @@ from .Entry_LabelsTkinter import Entry_LabelsTkinter
 class AppTkinter(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.Entry_Labels = Entry_LabelsTkinter(self)
         self.title("Gerenciamento de usuarios")
         self.geometry("960x540")
+
+        self.Entry_Labels = Entry_LabelsTkinter(self)
         self.__fonte_padrao: tuple = ("Arial", 16)
         self.__fonte_grande: tuple = ("Arial", 20)
         self.__fonte_pequena: tuple = ("Arial", 10)
 
-    def __create_widgets(self):
+    def __create_widgets(self) -> None:
         tk.Label(self, text="Gerenciamento de usuarios", font=self.__fonte_grande).grid(row=0, column=0, columnspan=3, pady=10)
         tk.Label(self, text="Preencha os campos abaixo:", font=self.__fonte_padrao).grid(row=1, column=0, columnspan=3, pady=5)
         self.Entry_Labels.createNomeEntry(self.__fonte_padrao, row=2, column=0)
