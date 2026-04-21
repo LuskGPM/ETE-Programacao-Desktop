93442e1675a6c4c8073670658f27040d130536aa
Lucas Melo <lucasgpm00@gmail.com>
2026-04-21 15:37:44 -0300

    Pinia Store criado

---
+++ src/src/store/piniaStore.ts
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+import { ref, type Ref } from "vue";
+import { ApiCep } from "@/services/api";
+
+export const useMainStore = defineStore('main', () => {
+    const jsonCep = ref({})
+
+    function getJsonCep(): any {
+        return jsonCep.value
+    }
+
+    async function setJsonCep(cep: string) {
+        jsonCep.value = await ApiCep.getCep(cep)
+    }
+
+    return { jsonCep, setJsonCep }
+})
