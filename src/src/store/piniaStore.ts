2adf46141ac3a94f92d723b7f77c255ccf8822cf
Lucas Melo <lucasgpm00@gmail.com>
2026-04-21 15:38:09 -0300

    Removendo o TYPE REF na importação do vue

--- src/src/store/piniaStore.ts
+++ src/src/store/piniaStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref, type Ref } from "vue";
+import { ref } from "vue";
 import { ApiCep } from "@/services/api";
 
 export const useMainStore = defineStore('main', () => {
